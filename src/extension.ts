a72ed094178266ae58f6665e58e290d2a24ad91c
Shao Yu-Lung (Allen) <bestlong168@gmail.com>
2025-04-17 22:58:50 +0800

    refactor: remove init command helloWorld

--- src/extension.ts
+++ src/extension.ts
@@ -32,17 +32,6 @@ export function activate(context: vscode.ExtensionContext) {
 	// This line of code will only be executed once when your extension is activated
 	console.log('Congratulations, your extension "vscode-grails" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('vscode-grails.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from vscode-grails!');
-	});
-
-	context.subscriptions.push(disposable);
-
     // 尋找對應的檔案
     const findDomainFile = (dirPath: string, targetFileName: string): string | null => {
         const files = fs.readdirSync(dirPath);
